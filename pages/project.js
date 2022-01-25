9b4367bb4fe59adefe7e51ffbff44e9e427611ec
MEHAK AGGARWAL <mehakagg1211@gmail.com>
2022-01-25 22:36:26 +0530

    changes

--- pages/project.js
+++ pages/project.js
@@ -37,11 +37,23 @@ export default function Modal() {
 
               <div class="flex items-center justify-center">
 
+
     <div className="flex border-2 rounded mb-24 border-orange-400">
         <input type="text"
          className="px-4 py-2 w-80" 
          placeholder="Search..."
+         onChange={(event)=>{
+           setSearchTerm(event.target.value);
+         }}
         />
+       {/* {Data.map((post)=>{
+          if(searchTerm ==""){
+            return post
+          }
+          else if(post.repo_fullname.toLowerCase().includes(searchTerm.toLowerCase())){
+            return post
+          }
+        })}*/}
         <button className="flex border-orange-400 items-center justify-center px-4 border-l">
             <svg className="border-orange-400 w-8 h-8 text-orange-600" fill="currentColor" xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24">
@@ -53,12 +65,111 @@ export default function Modal() {
 </div>
 </div>
 
-<div className="post">
+<div className="post flex-auto">
   {Data.map(post=> {
   return (
     <div key={post.project_id} className="post">
 
-      
+{showModal ? (
+  
+        <>
+        <div key={post.project_id} className="post">
+          <div
+            className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+          >
+            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+              {/*content*/}
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                {/*header*/}
+                <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
+                  <h3 className="text-3xl font-bold text-primary_orange-0">
+                  {post.project_name}
+                    <div className="font-bold text-xl text-slate-900	mt-2 font-serif	">
+                <a href={post.github} target="_blank">{post.repo_fullname}
+                </a>
+                </div>
+                  </h3>
+
+                  <button
+                    className="p-1 ml-auto bg-orange-400 rounded-lg shadow-xl shadow-orange-300 text-black float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                    onClick={() => setShowModal(false)}
+                  >
+                    <span className="bg-orange-400 text-white h-4 w-10 text-4xl block outline-none focus:outline-none mb-6">
+                      ×
+                    </span>
+                  </button>
+                </div>
+                {/*body*/}
+                <div className="relative p-6 flex-auto">
+                <h3 className="text-2xl font-bold text-primary_orange-0 mb-6">
+                    DESCRIPTION
+                  </h3>
+                 <p className="my-4 text-blueGray-500 text-lg leading-relaxed font-serif	">
+                 {post.project_description}
+                  </p>
+                </div>
+
+              <div className="relative p-6 flex-auto ">
+                
+              <div className="relative p-2 flex-auto float-right">
+                <h3 className="text-2xl font-bold text-primary_orange-0 mb-2">
+                    TECH STACK
+                    <div className=" w-28  mt-6 text-white font-serif rounded-md ml-2 ">
+                    <div className=" h-8 w-28 border-2 bg-orange-400 border-orange-400  text-lg block outline-none focus:outline-none mb-2 rounded-md pl-8">
+                    {post.technology_used}
+                    </div>
+                    
+                    </div>
+                  </h3>
+
+                  <h3 className="p-2 text-2xl font-bold text-primary_orange-0 mb-4">
+                    OWNER
+                  </h3>
+                 <p className="my-4 text-blueGray-500 text-lg leading-relaxed font-serif	">
+                   {post.owner_name}
+                  </p>
+
+                  <h3 className="text-2xl font-bold text-primary_orange-0 mb-2">
+                    ADMIN EMAIL
+                  </h3>
+                 <p className="my-4 text-blueGray-500 text-lg leading-relaxed font-serif	">
+                   {post.email}
+                  </p>
+
+                </div>
+                
+                <h3 className="text-2xl font-bold text-primary_orange-0 mb-2">
+                    PROJECT VIDEO
+
+                    <div className="font-bold text-lg text-primary_orange-0 mt-2 font-serif	">
+                <a href={post.project_video_link} target="_blank">
+                </a>
+                </div>
+                  </h3>
+
+                  <h3 className="text-2xl font-bold text-primary_orange-0 mb-2">
+                    MENTORS
+                    <div className=" w-28  mt-6 text-slate-900 font-serif rounded-md ml-2 ">
+                    <div className=" h-8 w-28 border-2 border-orange-400  text-lg block outline-none focus:outline-none mb-2 rounded-md pl-8">
+                      name
+                    </div>
+                    <div className=" h-8 w-28 border-2 border-orange-400  text-lg block outline-none focus:outline-none mb-2 rounded-md pl-8">
+                      name
+                    </div>      
+                    </div>
+                  </h3>
+              </div>
+              
+              </div>
+
+            </div>
+          </div>
+          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+
+          </div>
+        </>
+      ) : null}
+
 <div className="p-10 ">  
     <div className="max-w-sm rounded overflow-hidden shadow-xl shadow-orange-300">
       <div className="px-6 py-4">
@@ -68,7 +179,7 @@ export default function Modal() {
         type="button"
         onClick={() => setShowModal(true)}>explore</button>
     </div>
-        <div className="font-bold text-xl mb-6 text-primary_orange-0">{post.owner_name}</div>
+        <div className="font-bold text-xl mb-6 text-primary_orange-0">{post.project_name}</div>
         <p className="text-gray-700 text-base mt-2">
           {post.project_description}  </p>
       </div>
@@ -78,11 +189,15 @@ export default function Modal() {
       </div>
     </div>
   </div>
-    </div>
+  </div>
+
+
+    
   )
 
 })}
 </div>
+
 
 
 {/*modal */}
