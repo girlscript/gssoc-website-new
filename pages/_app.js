82c4523e1b42a1c80bb3b1140a214916d64e4dec
Gurjeet Singh Virdee <73753957+gurjeetsinghvirdee@users.noreply.github.com>
2022-04-27 03:55:40 +0530

    blog page created

--- pages/_app.js
+++ pages/_app.js
@@ -17,7 +17,6 @@ function MyApp({ Component, pageProps }) {
             showOnShallow={true}
             options={{ showSpinner: false }}
           />
-
           <Component {...pageProps} />
         </Layout>
       </ChakraProvider>
