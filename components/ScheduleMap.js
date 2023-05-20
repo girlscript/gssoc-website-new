ecab6474e07b353f5826c382df15efbaf8c015f9
Shubham Singh <116020663+1Shubham7@users.noreply.github.com>
2023-05-21 01:59:01 +0530

    Update ScheduleMap.js

--- components/ScheduleMap.js
+++ components/ScheduleMap.js
@@ -11,7 +11,7 @@ function Schedule() {
             Schedule{" "}
           </span>
           <span className="md:dark:bg-darkmode_gray-0 lg:bg-white pr-10 py-5">
-            of GSSoC 2022!
+            of GSSoC 2023!
           </span>
         </h1>
         <div data-aos="zoom-out" className="container">
