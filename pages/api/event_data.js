1d574a33241deb1ae9270a9a6b4dd77f35f77a13
Shristy Joshi Thakur <shristyjoshitk2001@gmail.com>
2024-05-10 01:27:33 +0530

    Update event_data.js

--- pages/api/event_data.js
+++ pages/api/event_data.js
@@ -1,71 +1,31 @@
 const items = [
   {
-    title: "5th May 2024",
+    title: "5th March 2024",
     poster:
-      "https://raw.githubusercontent.com/girlscript/gssoc-website-new/main/public/327992121-2df7a26e-44bf-4e97-8670-0bac03e3596a.jpg",
-    maintitle: "Conversation with Yati Padia",
+      "https://www.youtube.com/embed/A7PchMiBeN4?si=ngxDnZBdIfrdwRwI",
+    maintitle: "Kick Start with Opensource with Yati Padia",
+    rec_link: "https://www.youtube.com/live/A7PchMiBeN4?si=ngxDnZBdIfrdwRwI",
+    info: " Dive into real projects, gain hands-on experience, and make meaningful contributions alongside a supportive community of mentors and fellow developers.",
+    medium: "Youtube",
+    fmt:"video",
+  },
+  {
+    title: "9th May 2024",
+    poster:
+    "https://www.youtube-nocookie.com/embed/tMo1cgssjPQ?si=YPDofdcOxzAhrlbe",
+    maintitle: "GSSoC'24 Speaker Spotlight: Abhinav Awasthi",
     rec_link: "https://www.youtube.com/live/A7PchMiBeN4?si=EXJh3N80y3q1Zqfr",
-    info: "We were joined by Yati Padia as we dived into the conversations about open-source.",
-    medium: "Youtube",
-    fmt:"image",
-  },
-  {
-    title: "26th February 2022",
-    poster:
-      "https://user-images.githubusercontent.com/63473496/159972222-ecdae971-0c0f-48ca-a9a3-4f1d77ff26c6.png",
-    maintitle: "Open-Source Projects for GSSoC’22",
-    rec_link: "https://twitter.com/i/spaces/1ZkKzbnLpQDKv?s=20",
-    info: "An awesome Twitter Space was hosted by GSSoC’22 where we revealed some crazy interesting insights of the projects associated with GSSoC'22. The attendees got a chance to hear from project admins and get better understanding about their projects.",
-    medium: "Twitter",
-    fmt:"image",
-  },
-  {
-    title: "1st March 2022",
-    poster:
-      "https://www.youtube-nocookie.com/embed/kNcNKH1kCIM",
-    maintitle: `Opening Ceremony of GSSoC’22`,
-    rec_link: "https://youtu.be/kNcNKH1kCIM",
-    info: "GSSoC’22 started with a band as we conducted our opening ceremony. We had an insightful session with our Guest Speakers: Ms. Manaswini Das, Software Engineer at RedHat and Ms. Anubha Maneshwar, Founder of GirlScript Foundation.",
+    info: "Dive into his journey from internships at LinkedIn to mentoring 1000+ students. Ranked top 1.0% in India's competitive programming scene, Abhinav's insights are gold!",
     medium: "Youtube",
     fmt:"video",
   },
   {
-    title: "5th Match 2022",
+    title: "10th May 2024",
     poster:
-      "https://www.youtube.com/embed/AH3fLqHly_Q",
-    maintitle: "Community Bonding Session - Git & GitHub",
-    rec_link: "https://www.youtube.com/embed/AH3fLqHly_Q",
-    info: "GSSoC’22 hosted the Community Bonding Session to introduce open-source to beginners. We were privileged to host a beginner-friendly workshop on Git and GitHub under the guidance of Mr. Praveen Kumar and Mr. Rohan Kulkarni.",
-    medium: "Youtube",
-    fmt:"video",
-  },
-  {
-    title: "8th March 2022",
-    poster:
-      "https://user-images.githubusercontent.com/63473496/159974181-62292881-6bf9-4903-b630-6688786f6312.png",
-    maintitle: "Women’s Day Celebration at GSSoC’22",
-    rec_link: "https://twitter.com/i/spaces/1mnxedWlajbJX?s=20",
-    info: "To celebrate International Women’s Day, GSSoC’22 hosted a twitter space to have a conversation with Women In Tech. The speakers, Jahnavi Shah, Program Manager at GSSoC’21, Sujata, Lead Developer at JavaScript Mastery, and Ms. Mahak Makharia, Designer and Developer graced the occasion as they shared their experiences, and spoke about the peaks and valleys they have faced throughout their journey.",
-    medium: "Twitter",
-    fmt:"image",
-  },
-  {
-    title: "5th Match 2022",
-    poster:
-      "https://www.youtube.com/embed/yC_m_VOZ3Ow",
-    maintitle: "Community Bonding Session - Intro To BlockChain",
-    rec_link: "https://www.youtube.com/embed/yC_m_VOZ3Ow",
-    info: "Open Source has enhanced the technology growth speed exponentially. Currently one of the fastest-growing industries is BLOCKCHAIN. Bitcoin, Ethereum, NFT, To the moon ? Don\'t feel the FOMO of not knowing about it? We got you covered. Join us at GirlScript Summer of Code 2022 in a beginner-friendly session on Introduction to Blockchain with some experienced Blockchain Developers. Let\'s deep dive together, shall we? Speakers: Anmol Arora & Yash Garg",
-    medium: "Youtube",
-    fmt:"video",
-  },
-  {
-    title: "5th Match 2022",
-    poster:
-      "https://www.youtube.com/embed/cxWnls_HmP4",
-    maintitle: "How to Develop Games on Blockchain? presented by Moralis",
-    rec_link: "https://www.youtube.com/embed/cxWnls_HmP4",
-    info: "The buzzwords for the 21st century Blockchain, NFTs, Defi and what not. Blockchain Games are the next big thing in the Gaming sector. But how does one build one? Well that's what we have set up. A Hands-on coding experience where you will be able to code a complete blockchain game with Moralis and add it under your Projects Portfolio on Github. ",
+    "https://www.youtube-nocookie.com/embed/iWU8MXjedis?si=sSEmAZEBWgoDrB0l",
+    maintitle: "Hands-On GSSoC: A Beginner's Guide to Contributing Code",
+    rec_link: "https://www.youtube.com/live/iWU8MXjedis?si=sSEmAZEBWgoDrB0l",
+    info: "Join us for a live stream of Hands-On GSSoC: A Beginner's Guide to Contributing Code and kickstart your journey into the exciting realm of software development. ",
     medium: "Youtube",
     fmt:"video",
   },
